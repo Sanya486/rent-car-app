e3a7c206cea5109adf4b2e43787c86c60abc4b5e
Sanya486 <sanyok486@gmail.com>
2023-11-12 19:08:01 +0200

    github set ups

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename='/rent-car-app'>
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
